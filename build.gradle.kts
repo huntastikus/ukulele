3f3348bc903d3795b9acc7a2c31eaf88ef82eadd
huntastikus <76656129+huntastikus@users.noreply.github.com>
2024-08-11 02:48:18 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,17 +14,17 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
+    flatDir {
+        dirs("lib")
+    }
     maven { url = uri("https://m2.dv8tion.net/releases") }
     maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
-    // The 4.x version of JDA specifically needs a UDP fix from this commit:
-    // https://github.com/DV8FromTheWorld/JDA/commit/39ba0c2682ad99dbec88240cb8ea9d1ff7162ae9
-    // The snapshot version published in jitpack has this, so it is utilized for now.
-    // Eventually, a full upgrade to JDA 5.x will be necessary.
-    implementation("com.github.DV8FromTheWorld:JDA:legacy~v4-SNAPSHOT")
-    implementation("dev.arbjerg:lavaplayer:1.5.0")
+    implementation("com.github.DV8FromTheWorld:JDA:v5.0.0-beta.23")
+    implementation("dev.arbjerg:lavaplayer:2.1.2")
+    // implementation("com.github.lavalink-devs.lavaplayer-youtube-source:plugin:1.4.0")
     implementation(":youtube-plugin-1.4.0")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("com.github.aikaterna:lavaplayer-natives:original-SNAPSHOT")
