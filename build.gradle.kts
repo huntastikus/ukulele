6961c53af56401258f4ba5ebe45516fef07fcc23
huntastikus <huntastikus@gmail.com>
2024-12-07 22:23:32 +0100

    still repo issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,17 +14,18 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
-    flatDir {
-        dirs("lib")
-    }
-    maven { url = uri("https://m2.dv8tion.net/releases") }
-    maven { url = uri("https://jitpack.io") }
-    maven { url = uri("https://maven.lavalink.dev/releases") }
-    maven { url = uri("https://mvnrepository.com/artifact/com.github.DV8FromTheWorld/JDA") }
+    maven("https://jitpack.io")
+    //flatDir {
+    //    dirs("lib")
+    //}
+    //maven { url = uri("https://m2.dv8tion.net/releases") }
+    //maven { url = uri("https://jitpack.io") }
+    //maven { url = uri("https://maven.lavalink.dev/releases") }
+    //maven { url = uri("https://mvnrepository.com/artifact/com.github.DV8FromTheWorld/JDA") }
 }
 
 dependencies {
-    implementation("com.github.DV8FromTheWorld:JDA:5.0.0-beta.24")
+    implementation("com.github.DV8FromTheWorld:JDA:5.2.1")
     implementation("dev.arbjerg:lavaplayer:2.2.2")
     implementation("dev.lavalink.youtube:v2:1.8.3")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
