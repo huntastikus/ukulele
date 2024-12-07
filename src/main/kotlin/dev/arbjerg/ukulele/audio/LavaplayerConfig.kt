54bbe140cb64c90ba1a40a3f7378a25189ba7e62
huntastikus <huntastikus@gmail.com>
2024-12-08 00:54:47 +0100

    Testing again

--- src/main/kotlin/dev/arbjerg/ukulele/audio/LavaplayerConfig.kt
+++ src/main/kotlin/dev/arbjerg/ukulele/audio/LavaplayerConfig.kt
@@ -1,10 +1,11 @@
 package dev.arbjerg.ukulele.audio
 
 import com.sedmelluq.discord.lavaplayer.container.MediaContainerRegistry
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager
-import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers
 import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeSearchProvider
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager
 //import dev.lavalink.youtube.YoutubeAudioSourceManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -15,12 +16,9 @@ class LavaplayerConfig {
     fun playerManager(): AudioPlayerManager {
         val apm = DefaultAudioPlayerManager()
         val apiKey = System.getenv("YOUTUBE_API_KEY")
-        // Then add the rest, while excluding the legacy `YoutubeAudioSourceManager`
-        val youtubeSourceManager = YoutubeAudioSourceManager().apply {
-            setApiKey(apiKey)
-        }
+        
+        val youtubeSourceManager = YoutubeAudioSourceManager(true,apiKey,null)
         apm.registerSourceManager(youtubeSourceManager)
-        AudioSourceManagers.registerRemoteSources(apm, MediaContainerRegistry.DEFAULT_REGISTRY, YoutubeAudioSourceManager::class.java)
         return apm
     }
 }
